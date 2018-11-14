088dd488032a6b0d3e0cc11dde4b8f1d802534df
Rob <Rob@DESKTOP-2B1LI3H>
2018-11-15 10:10:21 +1100

    Final submit based on test cases working for Sub Array summer

--- src/test/java/subarraysumstotarget/SubArrayTester.java
+++ src/test/java/subarraysumstotarget/SubArrayTester.java
@@ -16,50 +16,43 @@ public class SubArrayTester {
 
 	@Test
 	public void test1() {
-		//fail("Not yet implemented");
 		int[] intarray = {12, 1, 12, 3, 1};
-		assertEquals("12,12",mySubArrayVerifier.verifySubArray(intarray,24));
+		assertEquals("[12, 12]",mySubArrayVerifier.verifySubArray(intarray,24));
 	}
 	
 	@Test
 	public void test2() {
-		//fail("Not yet implemented"); 
 		int[] intarray = {12, 1, 12, 3, 1};
-		assertEquals("12,3",mySubArrayVerifier.verifySubArray(intarray,15));
+		assertEquals("[12, 3]",mySubArrayVerifier.verifySubArray(intarray,15));
 	}
 	
 	@Test
 	public void test3() {
-		//fail("Not yet implemented");
 		int[] intarray = {12, 1, 8, 3, 1};
-		assertEquals("12,3,1,1",mySubArrayVerifier.verifySubArray(intarray,17));
+		assertEquals("[12, 3, 1, 1]",mySubArrayVerifier.verifySubArray(intarray,17));
 	}
 	
 	@Test
 	public void test4() {
-		//fail("Not yet implemented");
 		int[] intarray = {12, 1, 12, 3, 1};
-		assertEquals("5,4,3,2,1",mySubArrayVerifier.verifySubArray(intarray, 15));
+		assertEquals("[12, 3]",mySubArrayVerifier.verifySubArray(intarray, 15));
 	}
 	
 	@Test
 	public void test5() {
-		//fail("Not yet implemented");
 		int[] intarray = {1};
-		assertEquals("1",mySubArrayVerifier.verifySubArray(intarray,1));
+		assertEquals("[1]",mySubArrayVerifier.verifySubArray(intarray,1));
 	}
 	
 	@Test
-	public void test6() {
-		//fail("Not yet implemented"); 
+	public void test6() {		//fail("Not yet implemented"); 
 		int[] intarray = {2,2};
 		assertEquals("null",mySubArrayVerifier.verifySubArray(intarray,5));
 	}
 	
 	@Test
 	public void test7() {
-		//fail("Not yet implemented"); 
 		int[] intarray = {3,3,3,4,4,4,5,5,5};
-		assertEquals("12,12",mySubArrayVerifier.verifySubArray(intarray, 12));
+		assertEquals("[5, 4, 3]",mySubArrayVerifier.verifySubArray(intarray, 12));
 	}
 }
